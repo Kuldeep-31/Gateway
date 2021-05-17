518a9bde458b120de0f73add099bf60c451e4722
Kuldeep-31 <84117260+Kuldeep-31@users.noreply.github.com>
2021-05-17 14:00:45 -0400

    Create device.h

---
+++ device/device.h
@@ -0,0 +1,39 @@
+#ifndef DEVICE_H
+#define DEVICE_H
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
+
+#define PORT 49153 
+
+/*  structure to send device data */
+typedef struct dev_data{
+        unsigned int device_id;
+        float param_value;
+}Dev_Meta_Data;
+
+/* Class to send device data to gateway*/
+class Device
+{
+public:
+        /* Constructor */
+        Device();
+
+        /* Connect Gateway*/
+        int Connect_Server(void);
+
+        /* Send periodic data to gateway */
+        int Send_Data(int sock,unsigned int dev_id,float val);
+
+        /* Return socket descriptor */
+        int Get_Sockfd(void);
+private:
+        int m_device_sockfd;
+        struct sockaddr_in  m_dev_addr;
+        Dev_Meta_Data dev_data;
+};
+#endif
