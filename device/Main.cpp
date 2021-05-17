34aa0acd8b6c41b8e182bf3221256343756e3072
Kuldeep-31 <84117260+Kuldeep-31@users.noreply.github.com>
2021-05-17 14:01:39 -0400

    Add files via upload

---
+++ device/Main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "device.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	Device inst;
+	unsigned int dev_id =0;
+	float val = 0;
+	float range = 50.0;
+	if(argc != 2)
+	{
+		cout<<"Enter correct arguments"<<endl;
+		cout<<"device device_id "<<endl;
+		return -1;
+	}
+	else
+	{
+		while(1)
+		{
+			if( inst.Connect_Server() == 1 )
+			{
+				dev_id = atoi(argv[1]);
+				{
+					srand(time(0));
+					val = static_cast<float>(rand())/static_cast<float>(RAND_MAX)*range;
+					cout<<"Connection successful="<<dev_id<<" ,val="<<val<<endl;
+					int sock = inst.Get_Sockfd();
+					if(inst.Send_Data(sock,dev_id,val)>1)
+					{
+						cout<<"Send data to server"<<endl;
+						close(sock);
+					}
+					usleep(4000000);
+				}
+			}
+			else
+			{
+				break;
+			}
+		}
+	}
+	return 0;
+}
---
+++ device/device.cpp
@@ -0,0 +1,66 @@
+#include "device.h"
+#include <iostream>
+
+using namespace std;
+
+#define RETURN_SUCCESS 1
+#define RETURN_FAIL 0
+
+
+Device::Device():m_device_sockfd(0),m_dev_addr()
+{
+}
+
+int  Device::Connect_Server(void)
+{
+	int ret_status = RETURN_SUCCESS;
+	m_device_sockfd = socket(AF_INET,SOCK_STREAM,0);
+	if( m_device_sockfd < 0 )
+	{
+		cout<<"Client socket failed"<<endl;
+		ret_status = RETURN_FAIL;
+	}
+	
+	if( ret_status == RETURN_SUCCESS )
+	{
+		m_dev_addr.sin_family = AF_INET;
+		m_dev_addr.sin_port = htons(PORT);
+		
+		if(inet_pton(AF_INET,"127.0.0.1",&m_dev_addr.sin_addr) < 0 )
+		{	
+			ret_status = RETURN_FAIL;
+			cout<<"inet_pton failed"<<endl;
+		}
+		else
+		{
+			if(connect(m_device_sockfd,reinterpret_cast<struct sockaddr *>(&m_dev_addr),sizeof(m_dev_addr)) < 0 )
+			{
+				ret_status = RETURN_FAIL;
+				cout<<"connect failed"<<endl;	
+			}
+			
+		} 
+	}
+	
+	return (ret_status);
+}
+
+int Device::Send_Data(int sock, unsigned int dev_id, float val)
+{
+	int ret_status = RETURN_SUCCESS;
+	dev_data.device_id = dev_id;
+	dev_data.param_value = val;
+//	cout<<"val="<<dev_data.value;	
+	if( (ret_status = send(sock,reinterpret_cast<Dev_Meta_Data *>(&dev_data),sizeof(dev_data),0)) < 0)
+	{
+		ret_status =  RETURN_FAIL;
+	}
+	
+	return(ret_status);
+}
+
+int Device:: Get_Sockfd(void)
+{
+	return(m_device_sockfd);
+}
+
